88b3694566d5b3181a8ec8d8248cae096eec3bc0
JohnniePM <johnnie.jpm@outlook.com>
2023-06-08 14:21:14 +0200

    last

--- h.py
+++ h.py
@@ -10,20 +10,25 @@ import json
 def start():
     update=True
     while update:
-        version = json.load(open("version.json"))
+        version = json.load(open("version.json"))["version"]
         req = requests.post("http://127.0.0.1:8000")
         req_ver = json.loads(req.text)["pk"]
+        print("__________________Start__________________")
+        print("the version in the cloud:     " + str(req_ver))
+        print("Is there any newer version:?  "+str(req_ver > version))
+        print("installed version:            " + str(version))
+        print("__________________________UPDATE______________________________")
+        print("__________________________UPDATE______________________________")
+        print("__________________________UPDATE______________________________")
+        print("__________________End____________________\n\n")
         if req_ver > version:
             update=False
 
-        print("__________________Start__________________")
-        print("the version in the cloud:     " + str(req_ver))
-        print("Is there any newer version:?  "+str(req_ver >= version))
-        print("installed version:            " + str(version))
-        print("__________________End____________________\n\n")
 
         sleep(2)
 
 
 if __name__ == "__main__":
+    print("start")
     start()
+    print("end")
